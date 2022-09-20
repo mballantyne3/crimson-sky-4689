7beec0edcb8131c84a8426c005ea9e0aecf42567
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-19 21:47:04 -0600

    create dish show page in views with functioning spec

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -7,16 +7,18 @@ RSpec.describe 'dish show page' do
       @mac = Dish.create!(name: "Mac n Cheese", description: "cheesy goodness", chef_id: @chef_1.id)
       @cheddar = Ingredient.create!(name: "Cheddar", calories: 250, dish_id: @mac.id)
       @pasta = Ingredient.create!(name: "Pasta", calories: 150, dish_id: @mac.id)
+      DishIngredient.create!(dish_id: @mac.id, ingredient_id: @pasta.id)
+      DishIngredient.create!(dish_id: @mac.id, ingredient_id: @cheddar.id)
     end
 
     it 'displays a dish show page with name and description' do
       visit "/dishes/#{@mac.id}"
 
-      expect(page).to have_content("Mac n Cheese")
+      expect(page).to have_content(@mac.name)
       expect(page).to have_content("Description: #{@mac.description}")
 
       within("#ingredients") do
-        expect(page).to have_content("Ingredients Used: #{@cheddar.name}, #{@pasta.name}")
+        expect(page).to have_content("Ingredients Used:\n#{@cheddar.name} #{@pasta.name}")
       end
     end
   end
