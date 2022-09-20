42b4bc70cd268b03719bd251e30536f69a3e2f99
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-20 16:42:14 -0600

    add chef model test for ingredients used method

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+  describe "instance methods" do
+    it 'displays what ingredients were used by a specified chef' do
+      @chef_1 = Chef.create!(name: "MacKinley")
+      @mac = Dish.create!(name: "Mac n Cheese", description: "cheesy goodness", chef_id: @chef_1.id)
+      @cheddar = Ingredient.create!(name: "Cheddar", calories: 250, dish_id: @mac.id)
+      @pasta = Ingredient.create!(name: "Pasta", calories: 150, dish_id: @mac.id)
+      ChefIngredient.create!(chef_id: @chef_1.id, ingredient_id: @pasta.id)
+      ChefIngredient.create!(chef_id: @chef_1.id, ingredient_id: @cheddar.id)
+
+      expect(@chef_1.ingredients_used).to eq("Cheddar and Pasta")
+    end
+  end
 end
